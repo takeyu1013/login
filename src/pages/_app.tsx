c67b54bbb293c9aaf779ccb4c8048f79e1c2f577
takeyu1013 <yuto1013@gmail.com>
2022-06-04 12:07:39 +0900

    ✨

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,43 @@
+import type { Auth, User } from "firebase/auth";
 import type { AppProps } from "next/app";
 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { createContext, FC, ReactNode, useEffect, useState } from "react";
+
+import { app } from "../firebase";
+
+type Name = User["displayName"];
+
+const CONTEXT: {
+  auth: Auth;
+  name: Name;
+} = { auth: getAuth(app), name: null } as const;
+
+export const AuthContext = createContext(CONTEXT);
+
+const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const { auth } = CONTEXT;
+  const [name, setName] = useState<Name>(CONTEXT.name);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setName(user ? user.displayName : null);
+    });
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{ auth, name }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,13 @@
 import type { NextPage } from "next";
-import type { MouseEventHandler } from "react";
-import type { User } from "firebase/auth";
 
-import { useCallback, useEffect, useMemo, useState } from "react";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithRedirect,
-  signOut,
-} from "firebase/auth";
+import { useCallback, useMemo, MouseEventHandler, useContext } from "react";
+import { GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth";
 
-import { app } from "../firebase";
+import { AuthContext } from "./_app";
 
 const Home: NextPage = () => {
-  const { auth, provider } = useMemo(() => {
-    return { auth: getAuth(app), provider: new GoogleAuthProvider() };
-  }, []);
-  const [name, setName] = useState<User["displayName"]>(null);
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setName(user ? user.displayName : null);
-    });
-  }, [auth]);
+  const { auth, name } = useContext(AuthContext);
+  const provider = useMemo(() => new GoogleAuthProvider(), []);
 
   return (
     <div>
