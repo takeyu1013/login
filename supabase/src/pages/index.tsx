f4a9bd2b85a6a7369880227afa24c3193997a90d
takeyu1013 <yuto1013@gmail.com>
2022-06-04 23:14:13 +0900

    ✨

--- supabase/src/pages/index.tsx
+++ supabase/src/pages/index.tsx
@@ -1,10 +1,30 @@
+import type { User } from "@supabase/supabase-js";
 import type { NextPage } from "next";
+import type { MouseEventHandler } from "react";
+
+import { useCallback, useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 const Home: NextPage = () => {
+  const auth = supabase.auth;
+  const [name, setName] = useState<User["email"]>(undefined);
+  useEffect(() => {
+    auth.onAuthStateChange((_event, session) => {
+      setName(session && session.user ? session.user.email : undefined);
+    });
+  });
+  const login: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (event) => {
+      event.preventDefault();
+      auth.signIn({ provider: "google" });
+    },
+    [auth]
+  );
+
   return (
     <div>
-      <button>Login</button>
-      <p>Loading...</p>
+      <button onClick={login}>Login</button>
+      <p>{name}</p>
     </div>
   );
 };
