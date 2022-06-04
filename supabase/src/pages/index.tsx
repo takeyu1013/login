fb465df44ff2c33237d0d49dacdf7f83745717ae
takeyu1013 <yuto1013@gmail.com>
2022-06-04 21:29:48 +0900

    ✨

---
+++ supabase/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <button>Login</button>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Home;
