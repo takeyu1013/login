2d2f3507cc5fe6a6a1536e66c0baa1598c7d412e
takeyu1013 <yuto1013@gmail.com>
2022-06-08 23:11:29 +0900

    🚧

---
+++ clerk/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <button>Login</button>
+      <p>email</p>
+    </div>
+  );
+};
+
+export default Home;
