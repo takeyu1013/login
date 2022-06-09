4b655aa68e2c851c4f42df3ba8d1bd0e98cb50de
takeyu1013 <yuto1013@gmail.com>
2022-06-09 22:38:50 +0900

    ✨

--- clerk/src/pages/index.tsx
+++ clerk/src/pages/index.tsx
@@ -1,10 +1,39 @@
+import { useAuth, useSignIn, useUser } from "@clerk/nextjs";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const { signIn } = useSignIn();
+  const { signOut } = useAuth();
+  const { user, isSignedIn } = useUser();
+
   return (
     <div>
-      <button>Login</button>
-      <p>email</p>
+      {isSignedIn ? (
+        <div>
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              signOut();
+            }}
+          >
+            Logout
+          </button>
+          <p>{user.fullName}</p>
+        </div>
+      ) : (
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            signIn?.authenticateWithRedirect({
+              strategy: "oauth_google",
+              redirectUrl: "/",
+              redirectUrlComplete: "/",
+            });
+          }}
+        >
+          Login
+        </button>
+      )}
     </div>
   );
 };
